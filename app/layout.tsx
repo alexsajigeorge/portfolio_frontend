fb979b20444dfd313cf2e4506a4a92d5ff109205
Alex Saji <alex.saji@honorfx.com>
2024-03-14 16:00:00 +0400

    speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { FloatingNav } from "@/components/navbar/floating-navbar";
 import { TracingBeam } from "@/components/ui/tracing-beam";
@@ -30,6 +31,7 @@ export default function RootLayout({
         >
           <TracingBeam>
             {children}
+            <SpeedInsights />
             <FloatingNav />
           </TracingBeam>
         </ThemeProvider>

9ec91a08638ccde6bc391ae21cfd90df0d834f52
Alex Saji <alex.saji@honorfx.com>
2024-03-22 13:16:21 +0400

    images change

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ const products = [
     title: "Rogue",
     link: "https://userogue.com",
     thumbnail:
-      "https://images.unsplash.com/photo-1601445638532-3c6f6c3aa1d6?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1634245482486-01004231a957?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
